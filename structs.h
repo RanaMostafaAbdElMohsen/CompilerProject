3b6ba392098f478e3bc2768e74170236d193f028
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-04-21 05:04:52 -0700

    Add files via upload

---
+++ structs.h
@@ -0,0 +1,40 @@
+typedef enum { typeCon, typeId, typeOpr } nodeEnum;
+typedef enum { Integer, Float, Char, String, Bool, ConstIntger, ConstFloat, ConstChar, ConstString, ConstBool} typeEnum;
+typedef enum { Accepted, Redefined, OutOfScope} permission;
+/* constants */
+typedef struct {
+    typeEnum type;                  	/* type of constant */
+	char * value;						/* value of constant as char array */
+} conNodeType;
+
+/* identifiers */
+typedef struct {
+    int index;          				/* subscript to sym table  and brace table*/
+	typeEnum type; 						/* type */
+	permission per;
+	char * name;
+} idNodeType;
+
+/* operators */
+typedef struct {
+    int oper;                   		/* operator */
+    int nops;                   		/* number of operands */
+    struct nodeTypeTag *op[1];			/* operands, extended at runtime */
+} oprNodeType;
+
+typedef struct nodeTypeTag {
+    nodeEnum type;              		/* type of node */
+
+    union {
+        conNodeType con;        		/* constants */
+        idNodeType id;          		/* identifiers */
+        oprNodeType opr;        		/* operators */
+    };
+} nodeType;
+
+extern int symType[50];
+extern int symName[50];
+extern int symInit[50];
+extern int symBraces[50];
+extern int symUsed[50];
+
