f9bdc717898f12886a3517e6cea44a819c5f10b9
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-05-12 09:26:24 -0700

    Add files via upload

---
+++ Linked List/LinkedList.h
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include"structs.h"
+
+typedef struct SymTableData
+{
+    int symType;
+    int symInit;
+    int symUsed;
+    int symBraces;
+    char * symValue;
+     char * symName;
+    permission symPerm;
+
+}SymTableData;
+
+
+/* A linked list symTableNode */
+typedef struct symTableNode {
+    struct SymTableData * data;
+    int index;
+    struct symTableNode *next;
+} symTableNode;
+ 
+
+struct SymTableData* getSymTableData(int type, int init, int used, int brace,char * name,permission perm );
+void insertFirst(int index, struct SymTableData* data);
+struct symTableNode* deleteFirst();
+int length();
+bool isEmpty() ;
+struct SymTableData* find(int index);
+void printList();
+void setValue(int index, char * value);
+void setBrace(int findBrace);
+void setUsed(int findIndex);
+void setInit(int findIndex);
+void printUsed();
+void printNotUsed();
+void printInit();
+void printNotInit();
+bool nameUniqueInScope(char * name, int brace);
+int getIndex(char * name, int brace);
